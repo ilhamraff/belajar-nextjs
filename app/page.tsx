790d88d0e8b0127de3846b81f8fede792da5e60f
ilhamraff <ifadhilah261@gmail.com>
2025-04-20 02:33:35 +0700

    update workflows

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
     <div className="min-h-screen p-8">
       <h1 className="font-bold text-xl">Welcome Ilham.</h1>
       <h1 className="text-xl">CI/CD Success!!</h1>
+      <h1 className="text-xl">CI/CD Success!!</h1>
     </div>
   );
 }
