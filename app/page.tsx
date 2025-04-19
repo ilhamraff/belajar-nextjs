338beee4a727763b8c3f4fd3cd9ac6c4379d4725
ilhamraff <ifadhilah261@gmail.com>
2025-04-20 02:23:43 +0700

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="min-h-screen p-8">
       <h1 className="font-bold text-xl">Welcome Ilham.</h1>
-      <h1 className="text-xl">CI/CD Work!!</h1>
+      <h1 className="text-xl">CI/CD Success!!</h1>
     </div>
   );
 }
